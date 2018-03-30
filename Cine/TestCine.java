b1daa6859437197d9a5e5e5aafbcc80b3c703aff
KeanGx <keankyril@gmail.com>
2018-03-31 01:59:26 +0200

    Update TestCine.java

--- Cine/TestCine.java
+++ Cine/TestCine.java
@@ -1,4 +1,5 @@
 
+
 /**
  * Write a description of class TestCine here.
  *
@@ -15,7 +16,7 @@ public class TestCine{
         Kinepolis.venderAsiento(new Espectador("Gonzalo", 18, 20),InfinityWar);
         Kinepolis.venderAsiento(new Espectador("Sergio", 20, 21),InfinityWar);
         Kinepolis.venderAsiento(new Espectador("Alex", 18, 30),InfinityWar);
-        Kinepolis.venderAsiento(new Espectador("Pablo", 19, 50),InfinityWar);
+        Kinepolis.venderAsiento(new Espectador("Jimena", 19, 50),InfinityWar);
         Kinepolis.venderAsiento(new Espectador("Luis", 23, 40),InfinityWar);
         Kinepolis.venderAsiento(new Espectador("Guille", 18, 10),InfinityWar);
         Kinepolis.venderAsiento(new Espectador("Silvia", 23, 8),InfinityWar);
@@ -25,9 +26,9 @@ public class TestCine{
         Kinepolis.venderAsiento(new Espectador("Elena", 19, 50),InfinityWar);
         
         Kinepolis.MostrarSala();
-        System.out.println("--------------------------------------");
-        System.out.println("          INFORMACION SESION          ");
-        System.out.println("--------------------------------------");
+        System.out.println("-----------------------------------------");
+        System.out.println("            INFORMACION SESION           ");
+        System.out.println("-----------------------------------------");
         System.out.println("Pelicula: [" + InfinityWar.titulo + "]");
         System.out.println("Nº de Espectadores: "+ Kinepolis.cantEspectadores());
         System.out.println("Recaudacion: " + Kinepolis.recaudacion());
